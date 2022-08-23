a43edaecf0032f6bf16bf5b1339f394c7aa85d56
Isaack Mungui <munguiisaack@gmail.com>
2022-08-23 19:08:12 +0300

    Refactored code to evaluate negative values

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,18 +3,19 @@ import random
 number = random.randint(-10000, 10000)
 
 last_digit = abs(number) % 10
-if last_digit < 6:
+if number < 0:
+    print(
+        "Last digit of {} is {} and is greater than 5".format(number, -abs(last_digit))
+    )
+
+if last_digit < 6 and number > 0:
     print(
         "Last digit of {} is {} and is less than 6 and not 0"
         .format(number, last_digit)
     )
-elif last_digit > 5:
+elif last_digit > 5 and number > 0:
     print(
-        "Last digit of {} is {} and is greater than 5".format(number, last_digit)
+        "Last digit of {} is {} and is greater than 5".format(number, abs(last_digit))
     )
-elif number < 0:
-    print(
-        "Last digit of {} is {} and is less than 0".format(number, last_digit)
-    )
-else:
+elif last_digit == 0 and number > 0:
     print("Last digit of {} is {} and is 0".format(number, last_digit))
