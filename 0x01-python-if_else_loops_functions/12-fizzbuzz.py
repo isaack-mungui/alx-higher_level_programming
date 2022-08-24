f50c60dc7abbe842b2f6008c5baf12a28bb0392b
Isaack Mungui <munguiisaack@gmail.com>
2022-08-24 18:00:57 +0300

    Resolved error in output

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,8 +3,7 @@ def fizzbuzz():
     for num in range(1, 101):
         if num % 15 == 0:
             print("{}".format("Fizzbuzz"), end=' ')
-
-        if num % 3 == 0:
+        elif num % 3 == 0:
             print("{}".format("Fizz"), end=' ')
         elif num % 5 == 0:
             print("{}".format("Buzz"), end=' ')
