fae61a1f6f131ca67fa36c31d38835d1cc06c7c7
Isaack Mungui <munguiisaack@gmail.com>
2022-08-24 17:42:13 +0300

    Added function that prints the fizzbuzz sequence

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+def fizzbuzz():
+    for num in range(1, 101):
+        if num % 15 == 0:
+            print("FizzBuzz")
+
+        if num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
