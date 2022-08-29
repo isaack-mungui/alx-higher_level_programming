4272066c416f65645c02927a39c769286808664a
Isaack Mungui <munguiisaack@gmail.com>
2022-08-29 20:55:23 +0300

    Modified return values

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,9 @@
 def replace_in_list(my_list, idx, element):
     for i in range(len(my_list)):
         if i < 0:
-            pass
+            return my_list
         elif i > len(my_list):
-            pass
+            return my_list
 
         if i == idx:
             my_list[idx] = element
