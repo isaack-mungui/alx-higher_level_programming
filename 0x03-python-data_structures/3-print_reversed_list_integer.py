24d0d6c5523fc955085e7be5c8ad1e161061a455
Isaack Mungui <munguiisaack@gmail.com>
2022-08-29 21:57:58 +0300

    Added interpreter path

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,4 @@
+#!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     for i in reversed(range(len(my_list))):
         print(my_list[i])
-
-
-# res = print_reversed_list_integer([1, 2, 3, 4, 5])
-# print(res)
