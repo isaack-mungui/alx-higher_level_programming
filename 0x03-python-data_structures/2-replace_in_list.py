3788fe24690bd48ed8bfae02b81a89049ba63af5
Isaack Mungui <munguiisaack@gmail.com>
2022-08-29 21:03:49 +0300

    Refactored code: removed repeating clauses

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,10 +1,7 @@
 def replace_in_list(my_list, idx, element):
     for i in range(len(my_list)):
-        if i < 0:
+        if i < 0 or i > len(my_list):
             return my_list
-        elif i > len(my_list):
-            return my_list
-
-        if i == idx:
+        else:
             my_list[idx] = element
             return my_list
