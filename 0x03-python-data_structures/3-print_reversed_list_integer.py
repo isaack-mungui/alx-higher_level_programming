64b42c33ca6f60d65b4eec915abc821c903b67fe
Isaack Mungui <munguiisaack@gmail.com>
2022-08-29 21:56:52 +0300

    Added a function that prints integers of a list in reversed order

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,7 @@
+def print_reversed_list_integer(my_list=[]):
+    for i in reversed(range(len(my_list))):
+        print(my_list[i])
+
+
+# res = print_reversed_list_integer([1, 2, 3, 4, 5])
+# print(res)
