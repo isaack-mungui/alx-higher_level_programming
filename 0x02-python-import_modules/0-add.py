660d9c6840935251d92d06a4545ed10b6b9fbc3b
Isaack Mungui <munguiisaack@gmail.com>
2022-08-31 20:02:05 +0300

    Added declaration to avoid code execution when imported

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 from add_0 import add
 
-x = 1
-y = 2
-res = add(x, y)
-print("{} + {} = {}".format(1, 2, res))
+if __name__ == "__main__":
+    x = 1
+    y = 2
+    res = add(x, y)
+    print("{} + {} = {}".format(1, 2, res))
